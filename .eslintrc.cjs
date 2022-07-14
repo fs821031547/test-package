d98852ab982165582d316c97d6c72208dc598d13
fs821031547 <13723440051@163.com>
2022-07-14 11:34:35 +0800

    ci: add eslint and commitlint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard',
+    'eslint-config-standard-with-typescript',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: ['./tsconfig.json'],
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
